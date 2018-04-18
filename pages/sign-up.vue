a2259d98d29215d23f4f03f03146cbe0f7837349
许星 <1484006754@qq.com>
2018-04-18 19:42:30 +0800

    登录页面

---
+++ pages/sign-up.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>注册</div>
+</template>
+
+<script>
+    export default {
+        //修改当前页面的默认模板为sign.vue,若不修改默认为default.vue
+        layout:'sign',
+        head:{
+            title:'登录-简书',
+            meta:[
+                {name:'keywords',content:'登录界面'},
+                {name:'description',content:'登录简书'},
+                {name:'author',content:'许星'}
+            ]
+        },
+        data(){
+            return
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
